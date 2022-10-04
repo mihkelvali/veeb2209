53d7d128cb6eb22c914ac0dc63dc1751d1f2fa49
Mihkel Väli <mihkel.vali@hotmail.com>
2022-10-04 16:23:40 +0300

    Matkaklubi jätk

---
+++ loeng07/script.js
@@ -0,0 +1,10 @@
+let astendaja = 0;
+
+console.log('alustame astendamisega');
+
+while (astendaja < 100) {
+    console.log('2 astmes ' + astendaja + ' võrdub ' + 2 ** astendaja);
+    astendaja = astendaja + 1;
+}
+
+console.log('lõpetame astendamisega');
